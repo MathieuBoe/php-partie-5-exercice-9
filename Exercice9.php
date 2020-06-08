ca056c65fa0a09d915e6ca122f7622462b2c1bda
MathieuBoe <mathieuboe60@gmail.com>
2020-06-08 11:23:31 +0200

    Exercice complet

---
+++ Exercice9.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = 'Exercice9';
+
+include 'header.php';
+include '../Exercice5/Exercice5.php';
+foreach ($departement as $value){
+    echo ' '.$value;
+}
+
+include 'footer.php';?>
